63c7277abc628c6acf274563bc7a72697b8289ae
Leo <ronald.mauritsson@gmail.com>
2019-09-26 14:56:52 +0300

    added the myeach method with index and also the myselect method

--- main.rb
+++ main.rb
@@ -9,4 +9,19 @@ module Enumerable
       end
         self
     end
+
+    def my_each_with_index
+        i = 0
+      while i < self.size
+          yield(self[i], i)
+          i += 1
+          self
+      end
+    end
+
+    def my_select
+        result = []
+        self.my_each {|item| result.push(item) if yield(item)}
+        result
+    end
 end
