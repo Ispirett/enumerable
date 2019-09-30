b3d20b03a41691b3284aea9f6e01e4386b895d5d
Leo <ronald.mauritsson@gmail.com>
2019-09-30 20:41:35 +0300

    rubocop offenses auto corrected

--- main.rb
+++ main.rb
@@ -3,25 +3,25 @@
 module Enumerable
     def my_each
         i = 0
-      while i < self.size
-          yield (self[i])
-          i += 1
-      end
+        while i < self.size
+            yield (self[i])
+            i += 1
+        end
         self
     end
 
     def my_each_with_index
         i = 0
-      while i < self.size
-          yield(self[i], i)
-          i += 1
-          self
-      end
+        while i < self.size
+            yield(self[i], i)
+            i += 1
+            self
+        end
     end
 
     def my_select
         result = []
-        self.my_each {|item| result.push(item) if yield(item)}
+        self.my_each { |item| result.push(item) if yield(item) }
         result
     end
 end
